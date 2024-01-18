d934bc3d5ff97c53935617732620bfccac4d98a3
Aspic <149295894+Aspics@users.noreply.github.com>
2024-01-18 09:37:55 +0100

    Update quicksort.java

--- src/quicksort.java
+++ src/quicksort.java
@@ -26,5 +26,42 @@ public class quicksort {
         list.set(high, temp);
         return i + 1;
     }
+
+    private ArrayList<String> sort2(ArrayList<String> ar, int lo, int hi){
+        if (lo < hi){
+            int splitPoint = partition2(ar, lo, hi);
+            sort2(ar, lo, splitPoint);
+            sort2(ar, splitPoint +1, hi);
+        }
+        return ar;
+    }
+
+    private int partition2(ArrayList<String> ar, int lo, int hi){
+        String pivot = ar.get(lo);
+        lo--;
+        hi++;
+        while (true){
+            lo++;
+            while (lo<hi && ar.get(lo).compareTo(pivot) < 0){
+                lo++;
+            }
+            hi--;
+            while (hi>lo && ar.get(hi).compareTo(pivot) >= 0){
+                hi--;
+            }
+            if (lo<hi){
+                swap(ar, lo, hi);
+            }else {
+                return hi;
+            }
+        }
+    }
+
+    private ArrayList<String> swap(ArrayList<String> ar, int a, int b){
+        String temp = ar.get(a);
+        ar.set(a, ar.get(b));
+        ar.set(b, temp);
+        return ar;
+    }
     
 }
