93d9069f17c904dc5439bbae4ea03974c53f8ed7
rutile <johan_lefol@yahoo.com>
2024-01-18 09:43:59 +0100

    quicksort

--- src/quicksort.java
+++ src/quicksort.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class quicksort {
     //quicksort arraylist string input
-    public static void sort(ArrayList<String> list, int low, int high) {
+    public static void sort(ArrayList<PaireChaineEntier> list, int low, int high) {
         if (low < high) {
             int pi = partition(list, low, high);
             sort(list, low, pi - 1);
@@ -10,24 +10,24 @@ public class quicksort {
         }
     }
     //partition arraylist string input for quicksort
-    public static int partition(ArrayList<String> list, int low, int high) {
-        String pivot = list.get(high);
+    public static int partition(ArrayList<PaireChaineEntier> list, int low, int high) {
+        PaireChaineEntier pivot = list.get(high);
         int i = (low - 1);
         for (int j = low; j < high; j++) {
-            if (list.get(j).compareTo(pivot) < 0) {
+            if (list.get(j).getChaine().compareTo(pivot.getChaine()) < 0) {
                 i++;
-                String temp = list.get(i);
+                PaireChaineEntier temp = list.get(i);
                 list.set(i, list.get(j));
                 list.set(j, temp);
             }
         }
-        String temp = list.get(i + 1);
+        PaireChaineEntier temp = list.get(i + 1);
         list.set(i + 1, list.get(high));
         list.set(high, temp);
         return i + 1;
     }
 
-    private ArrayList<String> sort2(ArrayList<String> ar, int lo, int hi){
+    private ArrayList<PaireChaineEntier> sort2(ArrayList<PaireChaineEntier> ar, int lo, int hi){
         if (lo < hi){
             int splitPoint = partition2(ar, lo, hi);
             sort2(ar, lo, splitPoint);
@@ -36,17 +36,17 @@ public class quicksort {
         return ar;
     }
 
-    private int partition2(ArrayList<String> ar, int lo, int hi){
-        String pivot = ar.get(lo);
+    private int partition2(ArrayList<PaireChaineEntier> ar, int lo, int hi){
+        String pivot = ar.get(lo).getChaine();
         lo--;
         hi++;
         while (true){
             lo++;
-            while (lo<hi && ar.get(lo).compareTo(pivot) < 0){
+            while (lo<hi && ar.get(lo).getChaine().compareTo(pivot) < 0){
                 lo++;
             }
             hi--;
-            while (hi>lo && ar.get(hi).compareTo(pivot) >= 0){
+            while (hi>lo && ar.get(hi).getChaine().compareTo(pivot) >= 0){
                 hi--;
             }
             if (lo<hi){
@@ -57,8 +57,8 @@ public class quicksort {
         }
     }
 
-    private ArrayList<String> swap(ArrayList<String> ar, int a, int b){
-        String temp = ar.get(a);
+    private ArrayList<PaireChaineEntier> swap(ArrayList<PaireChaineEntier> ar, int a, int b){
+        PaireChaineEntier temp = ar.get(a);
         ar.set(a, ar.get(b));
         ar.set(b, temp);
         return ar;
